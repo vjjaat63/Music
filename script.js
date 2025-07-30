61f2cd8bf492645cd9b7fba3a81c5d4fc0c9fa1b
Vishal <vjjhajhria63@gmail.com>
2025-07-31 01:16:30 +0530

    added .m4a extension

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ let handledata = (data) => {
             const a = document.createElement('a');
             a.href = url;
             // Set filename to the real song name (sanitize for file systems)
-            a.download = `${name.replace(/[\\/:*?"<>|]/g, '')}.mp3`;
+            a.download = `${name.replace(/[\\/:*?"<>|]/g, '')}.m4a`;
             document.body.appendChild(a);
             a.click();
             document.body.removeChild(a);
